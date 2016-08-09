6d3e397d236fdbad5df1b2a8575d7c9543d298ec
nickianspencer <mrnicholasspencer@gmail.com>
2016-08-09 15:06:56 -0400

    adding gulp and sass compiler and ting

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+
+gulp.task('styles', function() {
+  return gulp.src('./styles/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest('./styles'))
+});
+
+gulp.task('watch', () => {
+  gulp.watch('./styles/**/*.scss', ['styles']);
+});
